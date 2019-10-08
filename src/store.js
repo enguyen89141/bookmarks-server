5ebe77a0a5cc893973f7cfd3f2f23e89ae50fb76
Eric Nguyen <enguyen89141@gmail.com>
2019-10-07 20:01:59 -0700

    finished inital submission

---
+++ src/store.js
@@ -0,0 +1,21 @@
+const uuid = require('uuid/v4')
+
+const bookmarks = [
+  { id: '114813ab-dc00-4d99-b00c-3f7a85c53f97',
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: 'c3364e4f-c0ef-4825-8335-7cdf5d710a1c',
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: '07398601-e37d-4825-aea1-e1e7b5c2a5e8',
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
+]
+
+module.exports = bookmarks
